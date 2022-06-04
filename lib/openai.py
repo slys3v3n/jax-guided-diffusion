0b5a27c6e96db7b5599131c0569e8b9f3eff51a2
huemin <100716027+huemin-art@users.noreply.github.com>
2022-06-04 16:48:28 -0700

    Update openai.py

--- lib/openai.py
+++ lib/openai.py
@@ -51,8 +51,31 @@ OPENAI_256_CONFIG = {
     'use_new_attention_order': False
 }
 
+OPENAI_256_KALIYUGA_CONFIG = {
+    'image_size': 256,
+    'in_channels': 3,
+    'model_channels': 128,
+    'out_channels': 6,
+    'num_res_blocks': 2,
+    'attention_resolutions': (16),
+    'dropout': 0.0,
+    'channel_mult': (1, 1, 2, 2, 4, 4),
+    'num_classes': None,
+    'use_checkpoint': False,
+    'use_fp16': False,
+    'num_heads': 1,
+    'num_head_channels': 64,
+    'num_heads_upsample': -1,
+    'use_scale_shift_norm': True,
+    'resblock_updown': True,
+    'use_new_attention_order': False
+}
+
 def create_openai_256_model(use_checkpoint=False, **kwargs):
     return partial(UNetModel, **OPENAI_256_CONFIG)(use_checkpoint=use_checkpoint, **kwargs)
+
+def create_openai_256_kaliyuga_model(use_checkpoint=False, **kwargs):
+    return partial(UNetModel, **OPENAI_256_KALIYUGA_CONFIG)(use_checkpoint=use_checkpoint, **kwargs)
 
 def create_openai_512_model(use_checkpoint=False, **kwargs):
     return partial(UNetModel, **OPENAI_512_CONFIG)(use_checkpoint=use_checkpoint, **kwargs)
